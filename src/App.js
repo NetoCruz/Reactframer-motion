1d0881f782e9c6100d844cb60dfcfb91cd1e895b
netocruz <galaxy.net0710@gmail.com>
2022-09-23 15:37:13 -0500

    animation examples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {motion} from 'framer-motion'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <motion.button
+       whileHover={{ scale: 1.3 }}
+       whileTap={{ scale: 0.9 }}
+      >click</motion.button>
+
+      <motion.div 
+       drag
+       dragMomentum={false}
+       dragConstraints={{
+         top: -50,
+         left: -50,
+         right: 50,
+         bottom: 50,
+       }
+      }
+      className='box'>jaja</motion.div>
+      <motion.div 
+      transition={{duration:2}}
+      animate={{
+        scale:[1,2,2,1,1],
+        rotate:[0,0,270,270,0],
+        borderRadius:["20%","20%","50%","50%","20%"]
+      
+      }} className='box'>
+        <p>animated</p>
+      </motion.div>
+      <motion.div 
+      initial={{scale:2}}
+      animate={{scale:1}} className='box'>
+        <p>hola</p>
+      </motion.div>
+      {/* <motion.h1 
+      initial={{
+        scale:2
+      }}
+      animate={{
+        y:100,
+        color:'white',
+        scale:1
+        }}>Hello</motion.h1> */}
     </div>
   );
 }
